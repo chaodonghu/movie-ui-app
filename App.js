16c2c62a7ef2052dfc1587e38bfbe32e97cb5784
chaodonghu <dong@applyboard.com>
2020-04-26 17:45:36 -0400

    add tab bar icons to navigation bar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 // Screens
 import Home from "./screens/home";
@@ -14,10 +15,47 @@ const Tab = createMaterialBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-        <Tab.Screen name="Recents" component={Recents} />
+      <Tab.Navigator
+        initialRoute="Home"
+        activeColor="#02ad94"
+        inactiveColor="#dedede"
+        style={{ backgroundColor: "#000" }}
+        barStyle={{ backgroundColor: "#0f0f0f", padding: 4 }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={28} />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="camera-metering-spot"
+                color={color}
+                size={28}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name="Recents"
+          component={Recents}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={28} />
+            )
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
